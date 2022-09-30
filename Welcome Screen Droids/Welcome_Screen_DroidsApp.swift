cc4b741a5b99304a44a3e7fd7db50eec50e6432f
Alekseienko <vvalekseienko@gmail.com>
2022-09-30 09:57:26 +0300

    Initial Commit

---
+++ Welcome Screen Droids/Welcome_Screen_DroidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_Screen_DroidsApp.swift
+//  Welcome Screen Droids
+//
+//  Created by alekseienko on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_Screen_DroidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
